182a037156864e66f59953e992fbbfe92925a042
Nicolai Christian Suhalim <65212050+nicolaics@users.noreply.github.com>
2024-10-24 15:49:03 +0900

    Delete frontend directory

--- frontend/lib/src/constants/image_strings.dart
+++
@@ -1,5 +0,0 @@
-const String WelcomeScreenImage =
-    "assets/images/welcomePage/welcome_screen.png";
-
-const String GoogleImg =
-    "assets/images/loginpage/google_icon.png";
--- frontend/lib/src/constants/sizes.dart
+++
@@ -1,5 +0,0 @@
-const tDefaultSize = 30.0;
-
-const tButtonHeight = 15.0;
-
-const tSplashContainerSize = 30.0;
