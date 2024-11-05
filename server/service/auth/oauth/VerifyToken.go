4fde2d5b8fdb96df8ae29b66d959d7289445d785
nicolaics <nicolai.christian.s@gmail.com>
2024-11-05 12:21:36 +0900

    remove comment

--- server/service/auth/oauth/VerifyToken.go
+++ server/service/auth/oauth/VerifyToken.go
@@ -12,11 +12,10 @@ var GoogleOauthConfig = &oauth2.Config{
 	ClientID:     config.Envs.GoogleClientID,
 	ClientSecret: config.Envs.GoogleClientSecret,
 	Endpoint:     google.Endpoint,
-	RedirectURL:  "ReURL", // 환경 변수에서 리디렉션 URL을 가져옵니다.
+	RedirectURL:  "ReURL", 
 }
 
 func VerifyIDToken(idToken string) (*idtoken.Payload, error) {
-	// ID 토큰 검증
 	payload, err := idtoken.Validate(oauth2.NoContext, idToken, GoogleOauthConfig.ClientID)
 	if err != nil {
 		return nil, err
@@ -24,12 +23,3 @@ func VerifyIDToken(idToken string) (*idtoken.Payload, error) {
 	
 	return payload, nil
 }
-
-//  {
-//    "access_token": "ya29.a0AfB_byBH_v_h2meyb_7pPScVR1SdtzQUE0JMcABLUDi-vZLfsVOGFtq_Ka3oz87fwpPjNT6A-IGW7a5woAKBs5yOza85P-eGrnv7pCPOwWfW1CSvV9JN-oRvVwkDFSs-7LtWZH30Aafi8Ata1TrxU0Hl7YF4R-h9jcUaCgYKAR0SARMSFQHGX2Mi8nS1YDw1toKOv3ttdzT0yg0170",
-//    "token_type": "Bearer",
-//    "expires_in": 3599,
-//    "scope": "email profile openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
-//    "authuser": "2",
-//    "prompt": "none"
-//}
