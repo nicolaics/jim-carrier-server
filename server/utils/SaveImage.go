b8e05d957f37acc1ee48bef5cacb58a78455c84f
nicolaics <nicolai.christian.s@gmail.com>
2024-11-07 15:50:12 +0900

    change max size into 10MB and change dir path

--- server/utils/SaveImage.go
+++ server/utils/SaveImage.go
@@ -5,33 +5,33 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+
+	"github.com/nicolaics/jim-carrier/constants"
 )
 
 func SaveProfilePicture(id int, imageData []byte, fileExtension string) (string, error) {
-	if err := os.MkdirAll("./static/profile_img/", 0744); err != nil {
+	if err := os.MkdirAll(constants.PROFILE_IMG_DIR_PATH, 0744); err != nil {
 		return "", err
 	}
 
-	// 5MB
-	maxBytes := 5 << 20 // 5MB in bytes
+	// 10MB
+	maxBytes := 10 << 20 // 10MB in bytes
 
 	// check for image size
 	if len(imageData) > maxBytes {
-		return "", fmt.Errorf("the image size exceeds the limit of 20MB")
+		return "", fmt.Errorf("the image size exceeds the limit of 10MB")
 	}
 
 	// set the image file name
 	randomNumber := GenerateRandomCodeNumbers(12)
 	fileName := fmt.Sprintf("%s-%d%s", randomNumber, id, fileExtension)
-	imagePath := "./static/profile_img/" + fileName
+	imagePath := constants.PROFILE_IMG_DIR_PATH + fileName
 
 	pattern := fmt.Sprintf(`-%d.(jpg|png)$`, id)
 	re := regexp.MustCompile(pattern)
 
-	folderPath := "./static/profile_img"
-
 	// Read all files in the specified folder
-	files, err := os.ReadDir(folderPath)
+	files, err := os.ReadDir(constants.PROFILE_IMG_DIR_PATH)
 	if err != nil {
 		return "", err
 	}
@@ -40,7 +40,7 @@ func SaveProfilePicture(id int, imageData []byte, fileExtension string) (string,
 		if !file.IsDir() {
 			if re.MatchString(file.Name()) {
 				// Full path of the file
-				fullPath := filepath.Join(folderPath, file.Name())
+				fullPath := filepath.Join(constants.PROFILE_IMG_DIR_PATH, file.Name())
 
 				// Delete the previous profile picture
 				err := os.Remove(fullPath)
@@ -70,20 +70,20 @@ func SaveProfilePicture(id int, imageData []byte, fileExtension string) (string,
 }
 
 func SavePaymentProof(imageData []byte, fileName string) error {
-	if err := os.MkdirAll("./static/payment_proof/", 0744); err != nil {
+	if err := os.MkdirAll(constants.PAYMENT_PROOF_DIR_PATH, 0744); err != nil {
 		return err
 	}
 
-	// 5MB
-	maxBytes := 5 << 20 // 5MB in bytes
+	// 10MB
+	maxBytes := 10 << 20 // 10MB in bytes
 
 	// check for image size
 	if len(imageData) > maxBytes {
-		return fmt.Errorf("the image size exceeds the limit of 20MB")
+		return fmt.Errorf("the image size exceeds the limit of 10MB")
 	}
 
 	// set the image file name
-	imagePath := "./static/payment_proof/" + fileName
+	imagePath := constants.PAYMENT_PROOF_DIR_PATH + fileName
 
 	// create the empty file for the image
 	file, err := os.Create(imagePath)
